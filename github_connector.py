88c9a24acecec6de2d73a67048d36c4659d27af8
Oliver Koenigsberger <oliver.koenigsberger@bmw.de>
2024-11-28 10:35:40 +0100

    Apply black formatter

--- github_connector.py
+++ github_connector.py
@@ -13,16 +13,19 @@ from github.Repository import Repository
 from constants import GITHUB_APP_ID
 
 # Constants
-PRIVATE_KEY_PATH = Path("github_app_private_key.pem").resolve()  # Path to your GitHub App's private key
+PRIVATE_KEY_PATH = Path("github_app_private_key.pem").resolve()
 
 
 class GitHubConnector:
-
     def __init__(self):
         self.installation: Installation = self.fetch_installation()
         private_key: str = self.read_private_key(PRIVATE_KEY_PATH)
-        self.github = self.authenticate_as_github_app(GITHUB_APP_ID, self.installation.id, private_key)
-        self.pr_id_to_check_run_id_dict: Dict[(int, str), int] = {}  # (pr_id, check_name) -> check_run_id
+        self.github = self.authenticate_as_github_app(
+            GITHUB_APP_ID, self.installation.id, private_key
+        )
+        self.pr_id_to_check_run_id_dict: Dict[
+            (int, str), int
+        ] = {}  # (pr_id, check_name) -> check_run_id
 
     @staticmethod
     def read_private_key(private_key_path: Path) -> str:
@@ -31,7 +34,9 @@ class GitHubConnector:
 
     @classmethod
     def fetch_installation(cls) -> Installation:
-        integration: GithubIntegration = GithubIntegration(GITHUB_APP_ID, cls.read_private_key(PRIVATE_KEY_PATH))
+        integration: GithubIntegration = GithubIntegration(
+            GITHUB_APP_ID, cls.read_private_key(PRIVATE_KEY_PATH)
+        )
         installations: PaginatedList[Installation] = integration.get_installations()
         for installation in installations:
             return installation
@@ -43,12 +48,21 @@ class GitHubConnector:
         return Github(installation_access_token.token)
 
     @staticmethod
-    def get_head_commit_sha_for_pull_request(repo: Repository, pull_request_id: int) -> str:
+    def get_head_commit_sha_for_pull_request(
+        repo: Repository, pull_request_id: int
+    ) -> str:
         pull_request: PullRequest = repo.get_pull(pull_request_id)
         return pull_request.head.sha
 
-    # Function to update the check run on GitHub
-    def update_checks_page(self, repo_name, check_name, pull_request_id, status, conclusion=None, output=None):
+    def update_checks_page(
+        self,
+        repo_name,
+        check_name,
+        pull_request_id,
+        status,
+        conclusion=None,
+        output=None,
+    ):
         repo: Repository = self.github.get_repo(repo_name)
         head_sha: str = self.get_head_commit_sha_for_pull_request(repo, pull_request_id)
         check_run_tuple: Tuple[int, str] = (pull_request_id, check_name)
@@ -60,30 +74,44 @@ class GitHubConnector:
                 check_run_id=check_run_id,
                 status=status,
                 conclusion=conclusion,
-                output=output)
+                output=output,
+            )
         else:
             check_run_id: int = self.init_checks_page(
                 repository=repo,
                 check_name=check_name,
                 commit_sha=head_sha,
                 status=status,
-                output=output)
+                output=output,
+            )
             self.pr_id_to_check_run_id_dict[check_run_tuple] = check_run_id
 
     @staticmethod
-    def init_checks_page(repository: Repository, check_name: str, commit_sha: str, status: str,
-                         output: Dict) -> int:
+    def init_checks_page(
+        repository: Repository,
+        check_name: str,
+        commit_sha: str,
+        status: str,
+        output: Dict,
+    ) -> int:
         check_run: CheckRun = repository.create_check_run(
             name=check_name,
             head_sha=commit_sha,
             status=status,
             started_at=datetime.datetime.utcnow(),
-            output=output)
+            output=output,
+        )
         check_run.update()
         return check_run.id
 
     @staticmethod
-    def edit_checks_page(repository: Repository, check_run_id: int, status: str, conclusion: str, output: Dict):
+    def edit_checks_page(
+        repository: Repository,
+        check_run_id: int,
+        status: str,
+        conclusion: str,
+        output: Dict,
+    ):
         check_run: CheckRun = repository.get_check_run(check_run_id)
         check_run.edit(
             name=check_run.name,
@@ -94,7 +122,7 @@ class GitHubConnector:
             started_at=check_run.started_at,
             conclusion=conclusion,
             completed_at=datetime.datetime.utcnow(),
-            output=output
+            output=output,
         )
         check_run.update()
 
@@ -102,48 +130,70 @@ class GitHubConnector:
 # Main function to be called with the necessary parameters
 def demo_github_connector():
     github = GitHubConnector()
-    repo_name = 'buildkite-demo-org/scheduler'  # Replace with your repository name
+    repo_name = "buildkite-demo-org/scheduler"  # Replace with your repository name
 
     # Check
-    output = {
-        'title': 'Check is running',
-        'summary': '...'
-    }
-    github.update_checks_page(repo_name=repo_name, check_name="check", pull_request_id=3, status="in_progress",
-                              conclusion=None, output=output)
+    output = {"title": "Check is running", "summary": "..."}
+    github.update_checks_page(
+        repo_name=repo_name,
+        check_name="check",
+        pull_request_id=3,
+        status="in_progress",
+        conclusion=None,
+        output=output,
+    )
 
     time.sleep(5)
 
     build_successful = True
     output = {
-        'title': 'Build Results',
-        'summary': 'The build completed successfully.' if build_successful else 'The build failed.'
+        "title": "Build Results",
+        "summary": "The build completed successfully."
+        if build_successful
+        else "The build failed.",
     }
     conclusion = "success" if build_successful else "failure"
-    github.update_checks_page(repo_name=repo_name, check_name="check", pull_request_id=3, status="completed",
-                              conclusion=conclusion, output=output)
+    github.update_checks_page(
+        repo_name=repo_name,
+        check_name="check",
+        pull_request_id=3,
+        status="completed",
+        conclusion=conclusion,
+        output=output,
+    )
 
     time.sleep(1)
 
     # Gate
-    output = {
-        'title': 'Gate is running',
-        'summary': '...'
-    }
-    github.update_checks_page(repo_name=repo_name, check_name="gate", pull_request_id=3, status="in_progress",
-                              conclusion=None, output=output)
+    output = {"title": "Gate is running", "summary": "..."}
+    github.update_checks_page(
+        repo_name=repo_name,
+        check_name="gate",
+        pull_request_id=3,
+        status="in_progress",
+        conclusion=None,
+        output=output,
+    )
 
     time.sleep(5)
 
     build_successful = False
     output = {
-        'title': 'Build Results',
-        'summary': 'The build completed successfully.' if build_successful else 'The build failed.'
+        "title": "Build Results",
+        "summary": "The build completed successfully."
+        if build_successful
+        else "The build failed.",
     }
     conclusion = "success" if build_successful else "failure"
-    github.update_checks_page(repo_name=repo_name, check_name="gate", pull_request_id=3, status="completed",
-                              conclusion=conclusion, output=output)
+    github.update_checks_page(
+        repo_name=repo_name,
+        check_name="gate",
+        pull_request_id=3,
+        status="completed",
+        conclusion=conclusion,
+        output=output,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     demo_github_connector()
